b2b9ac5c76eea5f4f85071555ecb6e9a0246d014
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 15:14:37 +0530

    Update Money.java
    constructor change

--- Money.java
+++ Money.java
@@ -2,18 +2,21 @@
 public abstract class Money {
 
 	public abstract Money times( int multiplier );
+	public abstract String currency();
 	protected int amount;
+	protected String currency;
 	
-	public Money( int amount ) {
+	public Money( int amount, String currency ) {
 		this.amount = amount;
+		this.currency = currency;
 	}
 	
 	public static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	
 	public static Money Franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 	
 }
