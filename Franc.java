512396bbad4948eecfe40a2be777feb667e44a4c
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 17:42:32 +0530

    Update Franc.java
    Removed Franc.java

--- Franc.java
+++ Franc.java
@@ -1,22 +1,23 @@
-
+/*
 public class Franc extends Money {
 	
 	public Franc( int amount, String currency ) {
 		super(amount, currency);
 	}
 
-	/*public Money times( int multiplier ) {
+	public Money times( int multiplier ) {
 		return new Money( amount * multiplier, currency );
 		//return Money.franc( amount * multiplier );
-	}*/
+	}
 	
-	/*public boolean equals( Object object ) {
+	public boolean equals( Object object ) {
 		Money money = ( Money ) object;
 		return this.amount == money.amount;
-	}*/
+	}
 	
-	/*public String currency() {
+	public String currency() {
 		return currency;
-	}*/
+	}
 
 }
+*/
