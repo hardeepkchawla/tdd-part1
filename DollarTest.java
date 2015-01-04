01f3294d51446c6fc1780740707f74e7d6e1eabc
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 17:41:37 +0530

    Update DollarTest.java
    Expression and Sum test methods included

--- DollarTest.java
+++ DollarTest.java
@@ -8,29 +8,55 @@ public class DollarTest {
 	@Test
 	public void testDollar() {
 		//fail("Not yet implemented");
-		testMultiplication();
-		testEquality();
+		//testDifferentClassEquality();
 	}
-	
+	@Test
 	public void testMultiplication() {
 		Money five = Money.dollar(5);
 		assertEquals( Money.dollar(10), five.times(2) );
 		assertEquals( Money.dollar(15), five.times(3) );
 	}
-	
+	@Test
 	public void testEquality() {
 		assertTrue( Money.dollar(5).equals(Money.dollar(5)) );
 		assertFalse( Money.dollar(5).equals(Money.dollar(6)) );
-		assertTrue( Money.Franc(5).equals(Money.Franc(5)) );
-		assertFalse( Money.Franc(5).equals(Money.Franc(6)) );
-		assertFalse(Money.Franc(5).equals(Money.dollar(6)));
+		assertTrue( Money.franc(5).equals(Money.franc(5)) );
+		assertFalse( Money.franc(5).equals(Money.franc(6)) );
+		assertFalse(Money.franc(5).equals(Money.dollar(6)));
 
 	}
-	
-	public void testCurrencty() {
-		assertEquals( "CHF", Money.Franc(1).currency() );
+	@Test
+	public void testCurrency() {
+		assertEquals( "CHF", Money.franc(1).currency() );
 		assertEquals( "USD", Money.dollar(1).currency() );
 	}
-
+	
+	/*@Test
+	  public void testDifferentClassEquality() {
+		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+	}*/
+	@Test
+	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Bank bank = new Bank();
+		Expression sum = five.plus(five);
+		Money reduced = bank.reduce(sum, five.currency() );
+		assertEquals(Money.dollar(10), reduced );
+	}
+	@Test
+	public void testPlusReturnsSum() {
+		Money five = Money.dollar(5);
+		Expression result = five.plus(five);
+		Sum sum = (Sum) result;
+		assertEquals(five, sum.augend );
+		assertEquals(five, sum.addend );
+	}
+	@Test
+	public void testReduceSum() {
+		Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+		Bank bank = new Bank();
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7), result);
+	}
 
 }
