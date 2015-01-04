6c020da586124e83a459574bc2d4ab2ecaf85301
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 16:21:33 +0530

    Update Franc.java
    Merge class into money

--- Franc.java
+++ Franc.java
@@ -5,17 +5,18 @@ public class Franc extends Money {
 		super(amount, currency);
 	}
 
-	public Money times( int multiplier ) {
-		return Money.Franc( amount * multiplier );
-	}
+	/*public Money times( int multiplier ) {
+		return new Money( amount * multiplier, currency );
+		//return Money.franc( amount * multiplier );
+	}*/
 	
-	public boolean equals( Object object ) {
+	/*public boolean equals( Object object ) {
 		Money money = ( Money ) object;
 		return this.amount == money.amount;
-	}
+	}*/
 	
-	public String currency() {
+	/*public String currency() {
 		return currency;
-	}
+	}*/
 
 }
