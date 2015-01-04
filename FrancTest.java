227e51163eb422157af878ac1c2e9145cde2c33f
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 17:42:54 +0530

    Update FrancTest.java
    Removed FrancTest

--- FrancTest.java
+++ FrancTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+/*import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -13,15 +13,17 @@ public class FrancTest {
 	}
 	
 	public void testMultiplication() {
-		Money five = Money.Franc(5);
-		assertEquals( Money.Franc(10), five.times(2) );
-		assertEquals( Money.Franc(15), five.times(3) );
+		Money five = Money.franc(5);
+		assertEquals( Money.franc(10), five.times(2) );
+		assertEquals( Money.franc(15), five.times(3) );
 	}
 	
 	public void testEquality() {
-		assertTrue( Money.Franc(5).equals(Money.Franc(5)) );
-		assertFalse( Money.Franc(5).equals(Money.Franc(6)) );
+		assertTrue( Money.franc(5).equals(Money.franc(5)) );
+		assertFalse( Money.franc(5).equals(Money.franc(6)) );
+		assertFalse( Money.franc(5).equals(Money.dollar(5)) );
 	}
 
 
 }
+*/
