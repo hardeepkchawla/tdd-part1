315c58d8aa9c4d9b8788ce941148d6ff74fc823f
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 14:47:31 +0530

    Create DollarTest.java

---
+++ DollarTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DollarTest {
+	
+	@Test
+	public void testDollar() {
+		//fail("Not yet implemented");
+		testMultiplication();
+		testEquality();
+	}
+	
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals( Money.dollar(10), five.times(2) );
+		assertEquals( Money.dollar(15), five.times(3) );
+	}
+	
+	public void testEquality() {
+		assertTrue( Money.dollar(5).equals(Money.dollar(5)) );
+		assertFalse( Money.dollar(5).equals(Money.dollar(6)) );
+		assertTrue( Money.Franc(5).equals(Money.Franc(5)) );
+		assertFalse( Money.Franc(5).equals(Money.Franc(6)) );
+		assertFalse(Money.Franc(5).equals(Money.dollar(6)));
+
+	}
+
+
+}
