ad2b281c9d9c8aa4c070449cffeb2cbd12ae7efd
hardeepkchawla <hardeep.kumar@globalnorthstar.com>
2015-01-04 19:08:21 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -12,4 +12,12 @@ public class Sum implements Expression {
 		int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
 		return new Money(amount, to);
 	}
+	
+	public Expression plus( Expression addend ) {
+		return new Sum( this, addend );
+	}
+	
+	public Expression times( int multiplier ) {
+		return new Sum( augend.times( multiplier ), addend.times( multiplier ) );
+	}
 }
